b1254cb950a1871705b418ebd4c20b413bad5ccc
samPuriwat <sam.puriwat@gmail.com>
2017-11-19 10:36:18 +0700

    Testing delete Information

--- src/oop_lab/is221/rmutsv/com/HelloWorld.java
+++ src/oop_lab/is221/rmutsv/com/HelloWorld.java
@@ -8,8 +8,8 @@ public static void main(String[] args) {
     System.out.println("Hello world, I'm SAM");
     System.out.println("My name is Puriwat Lertkrai");
 //    Add information
-    System.out.println("Faculty: Management Technology");
-    System.out.println("RMUTSV");
+    System.out.println("Faculty: Management Technology, RMUTSV");
+
 
 }//main
 
