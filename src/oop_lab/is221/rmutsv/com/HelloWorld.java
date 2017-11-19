72e87f7ec1035fbfdf6a9a7dc2d0e04b5f0ea1a7
samPuriwat <sam.puriwat@gmail.com>
2017-11-19 10:04:58 +0700

    Testing upload to Github

---
+++ src/oop_lab/is221/rmutsv/com/HelloWorld.java
@@ -0,0 +1,14 @@
+package oop_lab.is221.rmutsv.com;
+
+public class HelloWorld
+{
+//    Create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello world, I'm SAM");
+    System.out.println("My name is Puriwat Lertkrai");
+
+
+}//main
+
+}// class
