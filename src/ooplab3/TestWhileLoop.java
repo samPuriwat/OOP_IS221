d117cb3cd52164ac7c96382554f260ff58919da9
samPuriwat <sam.puriwat@gmail.com>
2017-12-04 14:55:46 +0700

    Nested Loop

---
+++ src/ooplab3/TestWhileLoop.java
@@ -0,0 +1,28 @@
+package ooplab3;
+
+public class TestWhileLoop {
+
+    public static void main(String[] args) {
+        // while loop
+        int n =1;
+//        while (n<=10){
+//
+//            System.out.println("Hello");
+//            n++;
+//
+//
+//        }//while
+
+//        do-while
+        do {
+            System.out.println("Hello");
+            n++;
+
+        } while (n<=10);
+
+
+    }//main
+
+
+
+}// class
