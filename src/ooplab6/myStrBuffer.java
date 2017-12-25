2a21d6c4652a6074dd0c1837007128b66727ba64
samPuriwat <sam.puriwat@gmail.com>
2017-12-25 18:12:21 +0700

    myStringBuffer

---
+++ src/ooplab6/myStrBuffer.java
@@ -0,0 +1,22 @@
+package ooplab6;
+
+public class myStrBuffer {
+    public static void main(String[] args) {
+//        StringBuffer
+
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("Hello");
+        buffer.append(" ");
+        buffer.append("to");
+        buffer.append(" ");
+        buffer.append("RMUTSV");
+        buffer.append(" ");
+        buffer.append(2017);
+        System.out.println(buffer);
+        buffer.replace(0, 5, "University of");
+        System.out.println(buffer);
+        System.out.println(buffer.reverse());
+        buffer.setCharAt(0,'T');
+        System.out.println(buffer);
+    }//main
+}//class
